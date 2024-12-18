21d00371312903daf3419977c5a1dcacc8d6d865
regis <regiscostapedro0705@gmail.com>
2024-12-17 22:43:26 -0300

    Fix: Passando o js para jquery

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-function menuShow() {
-  let menuMobile = document.querySelector(".mobile-menu");
-  let menuIcon = document.querySelector(".fa-bars");
+$('.menu-show').click(function(){
+    let menuMobile = document.querySelector(".mobile-menu");
+    let menuIcon = document.querySelector(".fa-bars");
+  
+    if (menuMobile.classList.contains("open")) {
+      menuMobile.classList.remove("open");
+  
+      menuIcon.classList.remove("fa-x");
+      menuIcon.style.color= "#2a65f0";
+    } else {
+        
+      menuMobile.classList.add("open");
+  
+  
+      menuIcon.classList.add("fa-x");
+      menuIcon.style.color = "#fff";
+      menuIcon.classList.remove(".fa-bars");
+    }
+})
 
-  if (menuMobile.classList.contains("open")) {
-    menuMobile.classList.remove("open");
-    menuIcon.classList.remove("fa-x");
-    menuIcon.style.color= "#2a65f0";
-  } else {
-    menuMobile.classList.add("open");
-    menuIcon.classList.add("fa-x");
-    menuIcon.style.color = "#555f";
-    menuIcon.classList.remove(".fa-bars");
-  }
-}
+
